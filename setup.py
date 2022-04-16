83a6e483061fcc2c6cb70f4ec3d1f7836883547b
NIKDISSV-Forever <nikdissv.forever@gmail.com>
2022-04-16 16:58:06 +0600

    PyPI commit

--- setup.py
+++ setup.py
@@ -7,12 +7,16 @@ with open('requires.txt', encoding='UTF-8') as f:
 
 setuptools.setup(
     name='spys',
-    version='0.0.1',
+    version='0.0.2',
     author='Nikita (NIKDISSV)',
     author_email='nikdissv.forever@protonmail.com',
     description='Python API for spys.one/spys.me proxies',
     long_description=long_description,
     long_description_content_type='text/markdown',
+    url='https://github.com/NIKDISSV-Forever/PySpysAPI',
+    package_dir={'': 'src'},
+    packages=setuptools.find_packages(where='src'),
+    requires=requires,
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
@@ -21,8 +25,5 @@ setuptools.setup(
         'Development Status :: 3 - Alpha',
         'Environment :: Console',
     ],
-    package_dir={'': 'src'},
-    packages=setuptools.find_packages(where='src'),
-    requires=requires,
     python_requires='>=3.8',
 )
