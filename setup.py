260c1e80dce81a3b9dc3ea599a2e760b6ae3f767
NIKDISSV-Forever <nikdissv.github.forever@gmail.com>
2022-08-19 12:55:38 +0600

    typing | bug fixes

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ import setuptools
 
 with open('README.md', encoding='UTF-8') as f:
     long_description = f.read()
-with open('requires.txt', encoding='UTF-8') as f:
+with open('requirements.txt', encoding='UTF-8') as f:
     requires = f.read().splitlines()
 
 setuptools.setup(
     name='spys',
-    version='1.0.5',
+    version='1.1.0',
     author='Nikita (NIKDISSV)',
-    author_email='nikdissv.forever@protonmail.com',
+    author_email='nikdissv@proton.me',
     description='Python API for spys.one/spys.me proxies',
     long_description=long_description,
     long_description_content_type='text/markdown',
