03ce9cf94829b5be814d2f6e739243b490dd9cea
NIKDISSV-Forever <nikdissv.github.forever@gmail.com>
2022-08-19 12:53:32 +0600

    bug & typing fixes

--- src/spys/me.py
+++ src/spys/me.py
@@ -1,19 +1,23 @@
-import re
+from __future__ import annotations
+
 import urllib.parse
 import urllib.request
-from typing import Tuple
+from re import compile as re_compile
 
-from spys.filters import filter_proxies
+from spys import filters
 from spys.proxy_view import BaseProxyView, ProxyViews
 
 __all__ = ('DATA_REGEX', 'HOST',
            'Getters',
-           'ProxyView', 'ResultType',
+           'ProxyView',
            'parse_proxies', 'get_proxies')
 
-ResultType = Tuple[str, ProxyViews, str]
 HOST = 'https://spys.me/'
-DATA_REGEX = re.compile(r'(\d+\.\d+\.\d+\.\d+):(\d+)\s+(..)-(.)-?(S?)(!?)\s+([+-]?)')
+DATA_REGEX = re_compile(r'(\d+\.\d+\.\d+\.\d+):(\d+)\s+(..)-(.)-?(S?)(!?)\s+([+-]?)')
+
+
+class UnknownProtocol(ValueError):
+    __slots__ = ()
 
 
 class ProxyView(BaseProxyView):
@@ -30,25 +34,29 @@ class ProxyView(BaseProxyView):
         self.google_passed = google_passed == '+'
 
 
-def parse_proxies(data: str) -> ResultType:
+def parse_proxies(data: str) -> tuple[str, ProxyViews, str]:
     """
     Returns a tuple from the additional information about API (str),
     list of proxies and additional proxy list information (str)
     """
     header, proxies_lines, footer = data.replace('\r', '').split('\n\n')
     return (header,
-            tuple(ProxyView(*data) for data in DATA_REGEX.findall(proxies_lines)),
+            (*(ProxyView(*data) for data in DATA_REGEX.findall(proxies_lines)),),
             footer)
 
 
 def get_content(protocol: str) -> str:
     """Send a request to the host, return the parsed information (see parse_proxies)"""
-    return urllib.request.urlopen(
-        urllib.request.Request(urllib.parse.urljoin(HOST, f'{protocol}.txt'),
-                               headers={'User-Agent': 'Mozilla/5.0'})).read().decode('utf-8')
+    with urllib.request.urlopen(
+            urllib.request.Request(urllib.parse.urljoin(HOST, f'{protocol}.txt'),
+                                   headers={'User-Agent': 'Mozilla/5.0'})) as resp:
+        return resp.read().decode()
 
 
 def _get_proxies(protocol: str) -> ProxyViews:
+    """
+    Protocol parameter - either "proxy" or "socks".
+    """
     return parse_proxies(get_content(protocol))[1]
 
 
@@ -56,23 +64,11 @@ def get_proxies(protocol: str = 'proxy') -> ProxyViews:
     """
     Protocol parameter - either proxy (http) or socks (socks5).
     Allowed protocols: proxy, socks | socks5, all, http, https, ssl (https & socks ssl)
-    :raise ValueError: if protocol is not allowed
     """
-    protocol = protocol.lower()
-    if protocol not in ('proxy', 'socks') and protocol in ('all', 'http', 'https', 'ssl', 'socks5'):
-        if protocol == 'all':
-            Getters.get_all_proxies()
-        elif protocol == 'http':
-            Getters.get_http_proxies()
-        elif protocol == 'https':
-            Getters.get_https_proxies()
-        elif protocol == 'socks5':
-            protocol = 'socks'
-        elif protocol == 'ssl':
-            return Getters.get_ssl_proxies()
-        else:
-            raise ValueError(f'Unknown protocol: {protocol!r}')
-    return _get_proxies(protocol)
+    protocol = protocol.lower().removesuffix('5')
+    return {'all': Getters.get_all_proxies,
+            'http': Getters.get_http_proxies, 'https': Getters.get_https_proxies,
+            'ssl': Getters.get_ssl_proxies}.get(protocol, lambda: _get_proxies(protocol))()
 
 
 class Getters:
@@ -85,11 +81,11 @@ class Getters:
 
     @staticmethod
     def get_http_proxies() -> ProxyViews:
-        return filter_proxies(get_proxies('proxy'), lambda x: not x.ssl_support)
+        return filters.filter_proxies(get_proxies('proxy'), lambda x: not x.ssl_support)
 
     @staticmethod
     def get_https_proxies() -> ProxyViews:
-        return filter_proxies(get_proxies('proxy'), lambda x: x.ssl_support)
+        return filters.filter_proxies(get_proxies('proxy'), lambda x: x.ssl_support)
 
     @staticmethod
     def get_socks5_proxies() -> ProxyViews:
@@ -101,5 +97,6 @@ class Getters:
         return cls.get_http_s_proxies() + cls.get_socks5_proxies()
 
     @classmethod
-    def get_ssl_proxies(cls, from_proxies: ProxyViews = None) -> ProxyViews:
-        return filter_proxies(cls.get_all_proxies() if from_proxies is None else from_proxies, lambda x: x.ssl_support)
+    def get_ssl_proxies(cls, from_proxies: ProxyViews = None) -> ProxyViews:  # socks with ssl and https
+        return filters.filter_proxies(cls.get_all_proxies() if from_proxies is None else from_proxies,
+                                      lambda x: x.ssl_support)
