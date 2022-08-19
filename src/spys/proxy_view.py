26f6b0968d233f16d123d76530b0b44a840beb63
NIKDISSV-Forever <nikdissv.github.forever@gmail.com>
2022-08-19 12:54:23 +0600

    new __repr__ and typing fixes

--- src/spys/proxy_view.py
+++ src/spys/proxy_view.py
@@ -1,8 +1,8 @@
-from typing import Callable, TypeVar, Union, Type, Iterable
+from __future__ import annotations
+
+from typing import Callable, Iterable, TypeVar
 
 __all__ = ('BaseProxyView', 'ProxyViews')
-
-import bs4
 
 TI = TypeVar('TI')
 TO = TypeVar('TO')
@@ -12,13 +12,13 @@ class BaseProxyView:
     __slots__ = ('host', 'port', 'anonymity', 'country',
                  'more_info')
     host: str
-    port: Union[int, str]
+    port: int | str
     anonymity: str
     country: str
-    more_info: Union[str, bool]
+    more_info: str | bool
 
     @staticmethod
-    def _convert_to(to_type: Callable[[TI], TO], val: TI) -> Union[TO, TI]:
+    def _convert_to(to_type: Callable[[TI], TO], val: TI) -> TO | TI:
         try:
             return to_type(val)
         except ValueError:
@@ -28,9 +28,9 @@ class BaseProxyView:
         return f'{self.host}:{self.port}'
 
     def __repr__(self) -> str:
-        return f'<{self.__class__.__name__}({self}) at {hex(id(self)).upper()}>'
+        return (
+            f"<{self.__class__.__qualname__}(ip='{self}', anm={self.anonymity!r}, country={self.country!r})"
+            f" at {hex(id(self))}>")
 
 
-ProxyViews: Type[Iterable[BaseProxyView]] = Iterable[BaseProxyView]
-
-
+ProxyViews = Iterable[BaseProxyView]
